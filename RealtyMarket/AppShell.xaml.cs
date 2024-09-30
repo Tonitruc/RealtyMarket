94ad4c0a4555cc9908d9a1802d9df0249e856260
Tonitrus <gromahacer@gmail.com>
2024-09-30 23:37:41 +0300

    Init commit

---
+++ RealtyMarket/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RealtyMarket
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RealtyMarket/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace RealtyMarket;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
